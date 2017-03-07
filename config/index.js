bad4cffb620a204eec4152900cce10ccfb8db711
Vaibhav <geekvaibhav@users.noreply.github.com>
2017-03-07 16:31:58 +0530

    Only Config Updates

--- config/index.js
+++ config/index.js
@@ -1,9 +1,9 @@
 const config = {}
 
-config.recastToken = 'Recast token'
-config.botToken = 'Bot token'
+config.recastToken = 'dccef3e283b71479163b3f8edad2e181'
+config.botToken = '0c5e71c6c192e5af5789e5c429ff4535'
 config.language = 'en'
 config.url= 'https://api-botconnector.recast.ai'
-config.botId = 'Bot id from bot-connector'
-config.slug = 'Usename of bot-connector'
+config.botId = '7df85da2-b550-46a6-987d-b199dec738da'
+config.slug = 'geekvaibhav'
 module.exports = config
